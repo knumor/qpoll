e959dda60189e7ef272652930c90b37321549e94
Morten Knutsen <knumor@gmail.com>
2024-10-01 08:20:57 +0200

    Restructure route setup

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ var staticFiles embed.FS
 
 func main() {
 	staticFs, _ := fs.Sub(staticFiles, "public")
+	mux := http.NewServeMux()
+	mux.Handle("GET /public/", http.StripPrefix("/public/", http.FileServer(http.FS(staticFs))))
+
 	pollStorage := storage.NewSQLiteStore()
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -27,18 +30,25 @@ func main() {
 		pollStorage.Close()
 		os.Exit(0)
 	}()
+
 	handlerContext := handlers.NewHandlerContext(pollStorage)
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /create/wordcloud", handlers.CreateWordCloudPage)
-	mux.HandleFunc("POST /create/wordcloud", handlerContext.CreateWordCloud)
-	mux.HandleFunc("GET /wordcloud/{id}", handlerContext.GetWordCloud)
-	mux.HandleFunc("GET /create", handlers.CreatePage)
-	mux.HandleFunc("GET /vote/{id}/", handlerContext.VotePage)
-	mux.HandleFunc("POST /vote", handlerContext.VoteSubmit)
-	mux.HandleFunc("POST /join", handlerContext.JoinExistingPoll)
-	mux.HandleFunc("GET /present/{id}", handlerContext.PresentPoll)
-	mux.HandleFunc("GET /qr/{id}", handlerContext.GenQRForPoll)
-	mux.Handle("GET /public/", http.StripPrefix("/public/", http.FileServer(http.FS(staticFs))))
-	mux.HandleFunc("GET /", handlers.JoinPollPage)
+	setupRoutes(mux, handlerContext)
+
 	_ = http.ListenAndServe("0.0.0.0:8080", mux)
+}
+
+func setupRoutes(mux *http.ServeMux, hc *handlers.HandlerContext) {
+	mux.HandleFunc("GET /create/wordcloud", handlers.CreateWordCloudPage)
+	mux.HandleFunc("POST /create/wordcloud", hc.CreateWordCloud)
+	mux.HandleFunc("GET /wordcloud/{id}", hc.GetWordCloud)
+	mux.HandleFunc("GET /create/multiple-choice", handlers.CreateMultipleChoicePage)
+	mux.HandleFunc("POST /create/mc", hc.CreateMultipleChoice)
+	mux.HandleFunc("GET /mc/{id}", hc.GetMultipleChoice)
+	mux.HandleFunc("GET /create", handlers.CreatePage)
+	mux.HandleFunc("GET /vote/{id}/", hc.VotePage)
+	mux.HandleFunc("POST /vote", hc.VoteSubmit)
+	mux.HandleFunc("POST /join", hc.JoinExistingPoll)
+	mux.HandleFunc("GET /present/{id}", hc.PresentPoll)
+	mux.HandleFunc("GET /qr/{id}", hc.GenQRForPoll)
+	mux.HandleFunc("GET /", handlers.JoinPollPage)
 }
