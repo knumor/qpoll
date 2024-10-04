d98d2d0283ac1f4c330a36a886a064fdd1a1651a
Morten Knutsen <knumor@gmail.com>
2024-10-04 14:56:18 +0200

    tailwind: Make sure all text-%dxl classes are available

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: {
     files: ["./views/**/*.go", "./components/**/*.go"],
   },
+  safelist: [
+    {
+      pattern: /text-[2-9]xl/,
+    },
+  ],
   theme: {
     extend: {},
   },
